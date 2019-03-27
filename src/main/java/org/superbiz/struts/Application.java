74fac4a2f3a9bef167708a739488a61305935ee5
apgovinda <apgovinda@gmail.com>
2019-03-27 12:23:29 +0530

    Struts_Solution

---
+++ src/main/java/org/superbiz/struts/Application.java
@@ -0,0 +1,41 @@
+package org.superbiz.struts;
+
+import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean SiteMeshFilter() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new SiteMeshFilter());
+        registration.addUrlPatterns("/*");
+        //registration.addInitParameter("paramName", "paramValue");
+        registration.setName("SiteMeshFilter");
+        registration.setOrder(0);
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean StrutsPrepareAndExecuteFilter() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new StrutsPrepareAndExecuteFilter());
+        registration.addUrlPatterns("/*");
+        //registration.addInitParameter("paramName", "paramValue");
+        registration.setName("StrutsPrepareAndExecuteFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+}
---
+++ src/main/java/org/superbiz/struts/Index.java
@@ -0,0 +1,18 @@
+package org.superbiz.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index extends ActionSupport {
+
+    @RequestMapping("/")
+    public String index() {
+        return "/index.jsp";
+    }
+
+
+
+}
+
